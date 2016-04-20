d51365c324fdaf562257da8154818a58439a546d
Parshuram <parushuram.k@huawei.com>
2016-04-20 08:28:52 +0000

    Converted relative path to absolute path

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 # This script expects mesos installation directory as mesos home directory
 # For no argument and invalid path the script will end
 
-MESOS_HOME_DIR="$1"
+MESOS_HOME="$1"
+# Will convert relative path to absolute path
+MESOS_HOME_DIR="$(dirname $(readlink -e $MESOS_HOME))/$(basename $MESOS_HOME)"
 
 if [ "$MESOS_HOME_DIR" == "" ]; then
   echo -e "\nPlease provide the Mesos Directory Path as argument\n"
@@ -21,7 +23,7 @@ UB_LIBPROCESS_PATH="$MESOS_HOME_DIR/3rdparty/libprocess"
 
 MESOS_LIB_PATH="-L $MESOS_HOME_DIR/build/src/.libs"
 
-HEADER_FILES=" -I $MESOS_HOME_DIR/include -I $MESOS_HOME_DIR/src -I $MESOS_HOME_DIR/build/include -I $MESOS_HOME_DIR/build/src -I $UB_LIBPROCESS_PATH/include  -I $UB_LIBPROCESS_PATH/3rdparty/stout/include -I $UB_LIBPROCESS_PATH/3rdparty/stout/include -I $B_THIRD_PARTY/picojson-1.3.0 -I $B_THIRD_PARTY/boost-1.53.0 -I$B_THIRD_PARTY/glog-0.3.3/src -I $B_THIRD_PARTY/stout/include -I $B_THIRD_PARTY/protobuf-2.6.1/src"
+HEADER_FILES=" -I $MESOS_HOME_DIR/include -I $MESOS_HOME_DIR/src -I $MESOS_HOME_DIR/build/include -I $MESOS_HOME_DIR/build/src -I $UB_LIBPROCESS_PATH/include  -I $UB_LIBPROCESS_PATH/3rdparty/stout/include -I $UB_LIBPROCESS_PATH/3rdparty/stout/include -I $B_THIRD_PARTY/picojson-1.3.0 -I $B_THIRD_PARTY/boost-1.53.0 -I$B_THIRD_PARTY/glog-0.3.3/src -I $B_THIRD_PARTY/stout/include -I $B_THIRD_PARTY/protobuf-2.5.8/src"
 
 $COMPILER $HEADER_FILES $MESOS_LIB_PATH -lmesos -std=c++11 -fPIC -shared ./FedAllocator/src/FedAllocator.cpp ./FedComm/src/FedComm.cpp -o libFedModules.so
 
